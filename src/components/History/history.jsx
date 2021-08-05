6b91aa2f03e94373949890a92dbff4882b95faf6
EggplantHero <jessetristanweber@gmail.com>
2021-08-05 18:38:54 -0400

    added delete tree button

--- src/components/History/history.jsx
+++ src/components/History/history.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import Tree from "./tree";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
+import { useDispatch } from "react-redux";
+import { getTrees, removeTree } from "../../store/trees";
 
 const History = () => {
   let { treeid } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleDelete = () => {
+    console.log("deleting...");
+    dispatch(removeTree(parseInt(treeid)));
+    history.push("/");
+  };
+
   return (
     <div className="container">
       <h6 className="text-center user-select-none mt-5">
         The optimal pattern was calculated for you based off given breeders:
       </h6>
       <Tree treeid={treeid} />
+      <div className="d-flex justify-content-center my-5">
+        <button className="btn btn-danger" onClick={handleDelete}>
+          Delete Tree From History
+        </button>
+      </div>
     </div>
   );
 };
